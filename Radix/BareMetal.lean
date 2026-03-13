03e7b9f0c62c134f733b45ce28867772eb84ef29
Aqua-218 <aqua@arivell.com>
2026-03-13 17:51:38 +0000

    feat(baremetal): add root BareMetal module re-exporting all submodules

---
+++ Radix/BareMetal.lean
@@ -0,0 +1,6 @@
+import Radix.BareMetal.Spec
+import Radix.BareMetal.GCFree
+import Radix.BareMetal.Linker
+import Radix.BareMetal.Startup
+import Radix.BareMetal.Lemmas
+import Radix.BareMetal.Assumptions
