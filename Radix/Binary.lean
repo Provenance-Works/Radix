9263f5c4b66bc0d106e92e16cfe012bfd7b46fa3
Aqua-218 <aqua@arivell.com>
2026-03-13 17:47:54 +0000

    feat(binary): add root Binary module re-exporting all submodules

---
+++ Radix/Binary.lean
@@ -0,0 +1,12 @@
+/-
+Copyright (c) 2026 Radix Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+import Radix.Binary.Spec
+import Radix.Binary.Format
+import Radix.Binary.Parser
+import Radix.Binary.Serial
+import Radix.Binary.Lemmas
+import Radix.Binary.Leb128
+import Radix.Binary.Leb128.Spec
+import Radix.Binary.Leb128.Lemmas
