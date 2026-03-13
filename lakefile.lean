1f1ea34a85b2e1e0882ae467539758a8eb27146a
Aqua-218 <aqua@arivell.com>
2026-03-13 17:17:02 +0000

    build(lakefile): pin mathlib to specific commit and add executable targets

--- lakefile.lean
+++ lakefile.lean
@@ -12,4 +12,17 @@ lean_lib Radix where
   srcDir := "."
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "master"
+  "https://github.com/leanprover-community/mathlib4" @ "06e947358d88e36af006f915f79a04a10fd43cc4"
+
+@[default_target]
+lean_exe test where
+  root := `tests.Main
+
+lean_exe proptest where
+  root := `tests.PropertyTests
+
+lean_exe bench where
+  root := `benchmarks.Main
+
+lean_exe examples where
+  root := `examples.Main
