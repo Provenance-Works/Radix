59368adc8cbbd1ea4facb0870eddfde087bef9de
Aqua-218 <aqua@arivell.com>
2026-03-13 17:40:47 +0000

    feat(bytes): add root Bytes module re-exporting all submodules

---
+++ Radix/Bytes.lean
@@ -0,0 +1,8 @@
+/-
+Copyright (c) 2026 Radix Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+import Radix.Bytes.Spec
+import Radix.Bytes.Order
+import Radix.Bytes.Slice
+import Radix.Bytes.Lemmas
