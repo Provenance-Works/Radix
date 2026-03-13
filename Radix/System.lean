172af4f241033ab7d60ff15c71ef71504086cffa
Aqua-218 <aqua@arivell.com>
2026-03-13 17:49:47 +0000

    feat(system): add root System module re-exporting all submodules

---
+++ Radix/System.lean
@@ -0,0 +1,9 @@
+/-
+Copyright (c) 2026 Radix Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+import Radix.System.Spec
+import Radix.System.Error
+import Radix.System.FD
+import Radix.System.IO
+import Radix.System.Assumptions
