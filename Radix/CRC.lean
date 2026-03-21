285ecf45a545a608c2a7e45b7479c19dd15ce71f
Aqua-218 <aqua@arivell.com>
2026-03-21 11:17:52 +0000

    chore(crc): add barrel import file

---
+++ Radix/CRC.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2026 Radix Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+import Radix.CRC.Spec
+import Radix.CRC.Ops
+import Radix.CRC.Lemmas
