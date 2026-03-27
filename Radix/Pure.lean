cdcb3fcba1ebc845d608ba55629febd067c80cf9
Aqua-218 <aqua@arivell.com>
2026-03-27 14:38:11 +0000

    feat(pure): add grouped import surface for pure modules

---
+++ Radix/Pure.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2026 Radix Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+import Radix.Word
+import Radix.Bit
+import Radix.Bytes
+import Radix.Memory
+import Radix.Binary
+import Radix.Alignment
+import Radix.RingBuffer
+import Radix.Bitmap
+import Radix.CRC
+import Radix.MemoryPool
+import Radix.UTF8
+import Radix.ECC
+import Radix.DMA
+import Radix.Timer
